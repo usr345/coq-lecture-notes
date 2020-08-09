ed0ad82e93b3eb8fc96540575e8ace28a6adb1ea
Ivanov Dmitriy <usr345@gmail.com>
2020-08-09 23:48:20 +0300

    temp

--- homework/hw08.v
+++ homework/hw08.v
@@ -15,7 +15,6 @@ Proof.
 Qed.
 
 
-
 Section TriFinType.
 (** Define [finType] structure for the following datatype *)
 (**
@@ -50,7 +49,106 @@ Qed.
 
 Definition tri_eq_mixin := EqMixin eq_tri_correct.
 
+(* Атья Макдональд *)
 Canonical tri_eqType := EqType tri tri_eq_mixin.
+
+(* Potan. *)
+Print choiceType.
+
+Definition tri_find (P: pred tri) (_: nat) : option tri :=
+  if P Yes
+  then Some Yes
+  else if P No
+       then Some No
+       else if P Maybe
+            then Some Maybe
+            else None.
+
+Lemma tri_find_correct1 : forall (P : pred tri) (n : nat) (x : tri), tri_find P n = Some x -> P x.
+Proof.
+  move => P ? x.
+  rewrite /tri_find.
+    by case x, (P Yes), (P No), (P Maybe).
+Qed.
+
+Lemma tri_find_correct2 : forall P : pred tri, (exists x : tri, P x) -> exists n : nat, tri_find P n.
+Proof.
+  move => P.
+  case => x C.
+  exists 0.
+  move : x C.
+  rewrite /tri_find.
+    by case x, (P Yes), (P No), (P Maybe).
+Qed.
+
+Lemma tri_find_correct3 : forall P Q : pred tri, P =1 Q -> tri_find P =1 tri_find Q.
+Proof.
+  move => P Q e ?.
+  rewrite /tri_find.
+  rewrite (e Yes) (e No) (e Maybe).
+  reflexivity.
+Qed.
+
+  Definition tri_choiceMixin := Choice.Mixin tri_find_correct1 tri_find_correct2 tri_find_correct3.
+
+  Canonical tri_choiceType := Eval cbn in ChoiceType tri tri_choiceMixin.
+End Potan.
+
+(* один из простых вариантов -- определить пару функций tri_to_ord и tri_of_ord (ord здесь значит "ординал"), доказать про них такую лемму *)
+Print ordinal.
+
+(** Your definitions here *)
+Definition tri2ord (t: tri) : 'I_3 :=
+  match t with
+  | Yes => (Ordinal (erefl : 0 < 3))
+  | No => (Ordinal (erefl : 1 < 3))
+  | Maybe => (Ordinal (erefl : 2 < 3))
+  end.
+
+Definition nat_of_ord (m : nat) (p : 'I_m) :=
+  let: @Ordinal _ n _ := p in n.
+
+Compute (nat_of_ord (tri2ord No)).
+
+Definition ord2tri (t: 'I_3) : tri :=
+  match (nat_of_ord t) with
+  | 0 => Yes
+  | 1 => No
+  | _ => Maybe
+  end.
+
+Print CanChoiceMixin.
+Lemma ordeqtri : cancel tri2ord ord2tri.
+case=> //.
+Qed.
+
+Definition bool_choiceMixin := CanChoiceMixin oddb.
+Definition tri_choiceMixin := CanChoiceMixin ordeqtri.
+Print bool_choiceMixin.
+Print tri_choiceMixin.
+Canonical bool_choiceType := Eval hnf in ChoiceType bool bool_choiceMixin.
+Canonical tri_choiceType := Eval hnf in ChoiceType tri tri_choiceMixin.
+Definition tri_countMixin := CanCountMixin ordeqtri.
+Canonical tri_countType := Eval hnf in CountType tri tri_countMixin.
+Definition tri_finMixin := CanFinMixin ordeqtri.
+Canonical tri_finType := Eval hnf in FinType tri tri_finMixin.
+
+(** This should work now: *)
+Check (Yes != No) && (No \in tri) && (#| tri | == 3).
+
+
+Definition tri_to_ord (x : tri) : ordinal :=
+
+Lemma tri_cancel: cancel tri_to_ord tri_of_ord.
+и воспользоваться рекомендацией из упражнения
+Definition tri_eqMixin := CanEqMixin tri_cancel.
+Canonical tri_eqType := EqType tri tri_eqMixin.
+Definition tri_choiceMixin := CanChoiceMixin tri_cancel.
+Canonical tri_choiceType := Eval hnf in ChoiceType tri tri_choiceMixin.
+Definition tri_countMixin := CanCountMixin tri_cancel.
+Canonical tri_countType := Eval hnf in CountType tri tri_countMixin.
+Definition tri_finMixin := CanFinMixin tri_cancel.
+Canonical tri_finType := Eval hnf in FinType tri tri_finMixin.
 
 (* Definition in_mem (x : tri) (T : Type) : bool := *)
 
